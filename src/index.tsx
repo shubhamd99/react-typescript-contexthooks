bff32e35a05fa5a88bbe6b7dfabcd4efb7123292
Shubham Dhage <shubhamdhage930@gmail.com>
2019-05-12 23:14:56 +0530

    changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 import { StoreProvider } from './store/Store';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,9 +8,9 @@ import 'tachyons';
 
 ReactDOM.render(
 <StoreProvider>
-    <BrowserRouter>
+    <Router>
         <App />
-    </BrowserRouter>
+    </Router>
 </StoreProvider>, document.getElementById('root'));
 
 
